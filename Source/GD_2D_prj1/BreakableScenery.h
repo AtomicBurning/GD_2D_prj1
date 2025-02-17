8131083f6e0e084fcdf008bcc7b0265f45d13e7f
Alexander Wood <24814752@stu.mmu.ac.uk>
2025-02-17 11:49:09 +0000

    Added Some Sprites

---
+++ Source/GD_2D_prj1/BreakableScenery.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "PaperSpriteComponent.h"
+#include "Components/BoxComponent.h"
+
+#include "BreakableScenery.generated.h"
+
+UCLASS()
+class GD_2D_PRJ1_API ABreakableScenery : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABreakableScenery();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visuals")
+	UPaperSpriteComponent* Sprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Object")
+	UBoxComponent* Box;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
